139eb4fb3eccb38cace82462e93815fcfba0018a
Aydin CESUR <152120171022@ogrenci.ogu.edu.tr>
2020-05-23 19:15:03 +0300

    Login Form added

--- Online Book Store/Online Book Store/Program.cs
+++ Online Book Store/Online Book Store/Program.cs
@@ -16,7 +16,7 @@ namespace Online_Book_Store
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login_Form());
         }
     }
 }
