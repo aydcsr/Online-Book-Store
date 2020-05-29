7ebd87e7897bf807e5e1159c944a527bb213a8f0
Aydin CESUR <152120171022@ogrenci.ogu.edu.tr>
2020-05-29 04:37:09 +0300

    MainForm

---
+++ Online Book Store/Online Book Store/MainForm.Designer.cs
@@ -0,0 +1,360 @@
+﻿namespace Online_Book_Store
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel13 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.panel11 = new System.Windows.Forms.Panel();
+            this.panel12 = new System.Windows.Forms.Panel();
+            this.panel14 = new System.Windows.Forms.Panel();
+            this.panel15 = new System.Windows.Forms.Panel();
+            this.panel16 = new System.Windows.Forms.Panel();
+            this.panel17 = new System.Windows.Forms.Panel();
+            this.panel18 = new System.Windows.Forms.Panel();
+            this.panel19 = new System.Windows.Forms.Panel();
+            this.panel20 = new System.Windows.Forms.Panel();
+            this.panel21 = new System.Windows.Forms.Panel();
+            this.panel22 = new System.Windows.Forms.Panel();
+            this.panel23 = new System.Windows.Forms.Panel();
+            this.panel24 = new System.Windows.Forms.Panel();
+            this.panel25 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Crimson;
+            this.panel1.Controls.Add(this.btnHide);
+            this.panel1.Controls.Add(this.btnClose);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1400, 40);
+            this.panel1.TabIndex = 0;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // btnHide
+            // 
+            this.btnHide.BackColor = System.Drawing.Color.Crimson;
+            this.btnHide.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHide.FlatAppearance.BorderSize = 0;
+            this.btnHide.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHide.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHide.ForeColor = System.Drawing.Color.White;
+            this.btnHide.Location = new System.Drawing.Point(1314, 0);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(40, 40);
+            this.btnHide.TabIndex = 2;
+            this.btnHide.Text = "_";
+            this.btnHide.UseVisualStyleBackColor = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Crimson;
+            this.btnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnClose.ForeColor = System.Drawing.Color.White;
+            this.btnClose.Location = new System.Drawing.Point(1360, -1);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(40, 40);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(671, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Online Book Store";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Silver;
+            this.panel2.Location = new System.Drawing.Point(0, 40);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(190, 680);
+            this.panel2.TabIndex = 3;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(196, 46);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(160, 200);
+            this.panel3.TabIndex = 4;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(362, 46);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(160, 200);
+            this.panel4.TabIndex = 5;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.panel13);
+            this.panel5.Location = new System.Drawing.Point(528, 46);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(160, 200);
+            this.panel5.TabIndex = 6;
+            // 
+            // panel13
+            // 
+            this.panel13.Location = new System.Drawing.Point(3, 206);
+            this.panel13.Name = "panel13";
+            this.panel13.Size = new System.Drawing.Size(160, 200);
+            this.panel13.TabIndex = 13;
+            // 
+            // panel6
+            // 
+            this.panel6.Location = new System.Drawing.Point(694, 46);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(160, 200);
+            this.panel6.TabIndex = 7;
+            // 
+            // panel7
+            // 
+            this.panel7.Controls.Add(this.panel8);
+            this.panel7.Location = new System.Drawing.Point(860, 46);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(160, 200);
+            this.panel7.TabIndex = 8;
+            // 
+            // panel8
+            // 
+            this.panel8.Location = new System.Drawing.Point(166, 0);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(160, 200);
+            this.panel8.TabIndex = 9;
+            // 
+            // panel9
+            // 
+            this.panel9.Location = new System.Drawing.Point(1026, 46);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(160, 200);
+            this.panel9.TabIndex = 9;
+            // 
+            // panel10
+            // 
+            this.panel10.Location = new System.Drawing.Point(1192, 46);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(160, 200);
+            this.panel10.TabIndex = 10;
+            // 
+            // panel11
+            // 
+            this.panel11.Location = new System.Drawing.Point(196, 252);
+            this.panel11.Name = "panel11";
+            this.panel11.Size = new System.Drawing.Size(160, 200);
+            this.panel11.TabIndex = 11;
+            // 
+            // panel12
+            // 
+            this.panel12.Location = new System.Drawing.Point(362, 252);
+            this.panel12.Name = "panel12";
+            this.panel12.Size = new System.Drawing.Size(160, 200);
+            this.panel12.TabIndex = 12;
+            // 
+            // panel14
+            // 
+            this.panel14.Location = new System.Drawing.Point(528, 252);
+            this.panel14.Name = "panel14";
+            this.panel14.Size = new System.Drawing.Size(160, 200);
+            this.panel14.TabIndex = 13;
+            // 
+            // panel15
+            // 
+            this.panel15.Location = new System.Drawing.Point(694, 252);
+            this.panel15.Name = "panel15";
+            this.panel15.Size = new System.Drawing.Size(160, 200);
+            this.panel15.TabIndex = 14;
+            // 
+            // panel16
+            // 
+            this.panel16.Location = new System.Drawing.Point(860, 252);
+            this.panel16.Name = "panel16";
+            this.panel16.Size = new System.Drawing.Size(160, 200);
+            this.panel16.TabIndex = 15;
+            // 
+            // panel17
+            // 
+            this.panel17.Location = new System.Drawing.Point(1026, 252);
+            this.panel17.Name = "panel17";
+            this.panel17.Size = new System.Drawing.Size(160, 200);
+            this.panel17.TabIndex = 16;
+            // 
+            // panel18
+            // 
+            this.panel18.Location = new System.Drawing.Point(1192, 252);
+            this.panel18.Name = "panel18";
+            this.panel18.Size = new System.Drawing.Size(160, 200);
+            this.panel18.TabIndex = 17;
+            // 
+            // panel19
+            // 
+            this.panel19.Location = new System.Drawing.Point(1192, 458);
+            this.panel19.Name = "panel19";
+            this.panel19.Size = new System.Drawing.Size(160, 200);
+            this.panel19.TabIndex = 24;
+            // 
+            // panel20
+            // 
+            this.panel20.Location = new System.Drawing.Point(1026, 458);
+            this.panel20.Name = "panel20";
+            this.panel20.Size = new System.Drawing.Size(160, 200);
+            this.panel20.TabIndex = 23;
+            // 
+            // panel21
+            // 
+            this.panel21.Location = new System.Drawing.Point(860, 458);
+            this.panel21.Name = "panel21";
+            this.panel21.Size = new System.Drawing.Size(160, 200);
+            this.panel21.TabIndex = 22;
+            // 
+            // panel22
+            // 
+            this.panel22.Location = new System.Drawing.Point(694, 458);
+            this.panel22.Name = "panel22";
+            this.panel22.Size = new System.Drawing.Size(160, 200);
+            this.panel22.TabIndex = 21;
+            // 
+            // panel23
+            // 
+            this.panel23.Location = new System.Drawing.Point(528, 458);
+            this.panel23.Name = "panel23";
+            this.panel23.Size = new System.Drawing.Size(160, 200);
+            this.panel23.TabIndex = 20;
+            // 
+            // panel24
+            // 
+            this.panel24.Location = new System.Drawing.Point(362, 458);
+            this.panel24.Name = "panel24";
+            this.panel24.Size = new System.Drawing.Size(160, 200);
+            this.panel24.TabIndex = 19;
+            // 
+            // panel25
+            // 
+            this.panel25.Location = new System.Drawing.Point(196, 458);
+            this.panel25.Name = "panel25";
+            this.panel25.Size = new System.Drawing.Size(160, 200);
+            this.panel25.TabIndex = 18;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1400, 720);
+            this.Controls.Add(this.panel19);
+            this.Controls.Add(this.panel20);
+            this.Controls.Add(this.panel21);
+            this.Controls.Add(this.panel22);
+            this.Controls.Add(this.panel23);
+            this.Controls.Add(this.panel24);
+            this.Controls.Add(this.panel25);
+            this.Controls.Add(this.panel18);
+            this.Controls.Add(this.panel17);
+            this.Controls.Add(this.panel16);
+            this.Controls.Add(this.panel15);
+            this.Controls.Add(this.panel14);
+            this.Controls.Add(this.panel12);
+            this.Controls.Add(this.panel11);
+            this.Controls.Add(this.panel10);
+            this.Controls.Add(this.panel9);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel7.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel13;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Panel panel11;
+        private System.Windows.Forms.Panel panel12;
+        private System.Windows.Forms.Panel panel14;
+        private System.Windows.Forms.Panel panel15;
+        private System.Windows.Forms.Panel panel16;
+        private System.Windows.Forms.Panel panel17;
+        private System.Windows.Forms.Panel panel18;
+        private System.Windows.Forms.Panel panel19;
+        private System.Windows.Forms.Panel panel20;
+        private System.Windows.Forms.Panel panel21;
+        private System.Windows.Forms.Panel panel22;
+        private System.Windows.Forms.Panel panel23;
+        private System.Windows.Forms.Panel panel24;
+        private System.Windows.Forms.Panel panel25;
+    }
+}
---
+++ Online Book Store/Online Book Store/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Online_Book_Store
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        int Move;
+        int Mouse_X;
+        int Mouse_Y;
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Move = 0;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Move = 1;
+            Mouse_X = e.X;
+            Mouse_Y = e.Y;
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (Move == 1)
+            {
+                this.SetDesktopLocation(MousePosition.X - Mouse_X, MousePosition.Y - Mouse_Y);
+            }
+        }
+    }
+}
